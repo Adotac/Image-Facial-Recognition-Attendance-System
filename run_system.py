4e2eaecf39f277b11ef119f610167cd81a5077cc
Adotac <otacool89@gmail.com>
2022-04-29 11:56:00 +0800

    Added real time display

--- run_system.py
+++ run_system.py
@@ -3,3 +3,95 @@
 ###
 
 ## The main program
+import tkinter as tk
+from time import strftime
+import cv2
+import PIL.Image, PIL.ImageTk
+
+class App:
+    def __init__(self, window, window_title, video_source=0):
+        self.window = window
+        self.window.title(window_title)
+        self.window.geometry("1000x520+300+100")
+        self.window.resizable(width=False, height=False)
+        self.video_source = video_source
+        self.ok = False
+
+        # open video source (by default this will try to open the computer webcam)
+        self.vid = VideoCapture(self.video_source)
+        # Create a canvas that can fit the above video source size
+        self.canvas = tk.Canvas(window, width=640, height=480)
+        # self.canvas.place(x=20, y=20)
+        self.canvas.pack(side=tk.LEFT)
+
+        self.timeDate = tk.Label(window, font=('times', 26, 'bold'), bg='yellow')
+        self.timeDate.place(x=645, y=10, width=400)
+        self.TimeDate()
+
+        # Button that lets the user take a snapshot
+        self.btn_snapshot = tk.Button(window, text="Check Attendance", command=self.snapshot)
+        self.btn_snapshot.pack(side=tk.LEFT)
+
+        # quit button
+        self.btn_quit = tk.Button(window, text='QUIT', command=quit)
+        self.btn_quit.pack(side=tk.LEFT)
+
+        # After it is called once, the update method will be automatically called every delay milliseconds
+        self.delay = 10
+        self.update()
+
+        self.window.mainloop()
+
+    def snapshot(self):
+        # Get a frame from the video source
+        ret, frame = self.vid.get_frame()
+        # if ret:
+        #     cv2.imwrite("frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+
+    def update(self):
+        # Get a frame from the video source
+        ret, frame = self.vid.get_frame()
+
+        if ret:
+            self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
+            self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
+        self.window.after(self.delay, self.update)
+
+    def TimeDate(self):
+        time_string = strftime('%H:%M:%S %p \n %A, %x')  # time format
+        self.timeDate.config(text=time_string)
+        self.timeDate.after(1000, self.TimeDate)  # time delay of 1000 milliseconds
+
+
+class VideoCapture:
+    def __init__(self, video_source=0):
+        # Open the video source
+        self.vid = cv2.VideoCapture(video_source)
+        if not self.vid.isOpened():
+            raise ValueError("Unable to open video source", video_source)
+
+    # To get frames
+    def get_frame(self):
+        if self.vid.isOpened():
+            ret, frame = self.vid.read()
+            if ret:
+                # Return a boolean success flag and the current frame converted to BGR
+                return (ret, cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+            else:
+                return (ret, None)
+        else:
+            return (None, None)
+
+    # Release the video source when the object is destroyed
+    def __del__(self):
+        if self.vid.isOpened():
+            self.vid.release()
+            cv2.destroyAllWindows()
+
+
+def main():
+    # Create a window and pass it to the Application object
+    App(tk.Tk(), 'Attendance System')
+
+if __name__ == "__main__":
+    main()
